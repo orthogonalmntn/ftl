da489da39f3ed9cd35f6540d617db3d070e3b975
orthogonalmntn <orthogonalmntn@no.mail>
2022-04-03 17:42:58 +0200

    Switch to a topic exchange for message publishing

--- lib/events/messaging/producer.rb
+++ lib/events/messaging/producer.rb
@@ -8,7 +8,7 @@ module Events
 
       ERROR_MESSAGES = [
         "ERR: Failed to connect to RabbitMQ Server!. Make sure RabbitMQ is running and properly configured.",
-        "ERR: Events will *not* be transmitted."
+        "ERR: Events will *not* be published to StatsService."
       ].freeze
 
       def initialize
@@ -16,16 +16,18 @@ module Events
         connection.start
 
         @channel = connection.create_channel
-        @queue = @channel.queue("game_events")
+        @exchange = @channel.topic("game_events")
       rescue Bunny::TCPConnectionFailed
         ERROR_MESSAGES.each { puts in_red _1 }
       end
 
       def emit_event(payload)
-        return unless @channel
+        return unless @exchange
 
+        routing_key = routing_key_for(payload)
         serialized_payload = build_payload(payload)
-        @channel.default_exchange.publish(serialized_payload, routing_key: @queue.name)
+
+        @exchange.publish(serialized_payload, routing_key: routing_key)
       end
 
       private
@@ -34,6 +36,19 @@ module Events
         payload.merge!(
           timestamp: Time.now,
         ).to_json
+      end
+
+      def routing_key_for(payload)
+        routing_key = case payload[:type].to_s
+                      when /Planet/
+                        "game_events.planets"
+                      when /Ship/
+                        "game_events.ships"
+                      when /Shop/
+                        "game_events.shops"
+                      end
+
+        routing_key
       end
 
     end
